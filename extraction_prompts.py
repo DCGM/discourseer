9dfca6956677b39fc465d18a47a1cf9a874844a7
vlachvojta <vojtik.vlach@seznam.cz>
2024-02-22 20:50:24 +0100

    Add prompt examples with options.

--- extraction_prompts.py
+++ extraction_prompts.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import pydantic
 from typing import List, Tuple
 
@@ -6,6 +7,18 @@ class ExtractionPrompts(pydantic.BaseModel):
     name: str
     description: str
     examples: List[Tuple[str, str]]
+    options: List[ResultOption] = []
+
+    def get_description(self) -> str:
+        return self.description + " " + " ".join(str(option) for option in self.options)
+
+
+class ResultOption(pydantic.BaseModel):
+    name: str
+    description: str
+
+    def __str__(self) -> str:
+        return self.name + " (" + self.description + ")"
 
 
 extraction_prompts = {
@@ -20,4 +33,25 @@ extraction_prompts = {
     "organizations": ExtractionPrompts(name="organizations", description="Organizations include states, companies, institutions, associations, and other groups of people with a common purpose or identity.", examples=[]),
     "perpetrators": ExtractionPrompts(name="perpetrators", description="Perpetrators should include the exact words or phrases that identify the person or group of people who committed a crime, act of violence, or other harmful act.", examples=[]),
     "victims": ExtractionPrompts(name="victims", description="Victims should include the exact words or phrases that identify the person or group of people who were harmed by a crime, act of violence, or other harmful act.", examples=[]),
+
+    "5-range": ExtractionPrompts(name="5-range", description="Number of paragraphs in the text.", examples=[]),
+    "6-genre": ExtractionPrompts(name="6-genre", description="The genre of the text with these options:", examples=[],
+                                 options=[ResultOption(name="1-report", description="Report is information about a current event answering questions: who, what, where."),
+                                          ResultOption(name="2-extended_report", description="Report is information about a current event answering questions: who, what, where, when, how and why."),
+                                          ResultOption(name="3-interview", description="Interview is a conversation between two or more people."),
+                                          ResultOption(name="9-other", description="Other genre.")]),
+    "8-message-trigger": ExtractionPrompts(name="8-message-trigger", description="The message trigger is the event or situation that causes the message to be sent. It has these options:", examples=[],
+                                           options=[ResultOption(name="1-politician", description="The message reacts on actions of a politician"),
+                                                    ResultOption(name="2-security-forces", description="The message reacts on action of security forces."),
+                                                    ResultOption(name="4-public", description="The message reacts on action of public."),
+                                                    ResultOption(name="9-other", description="Other message trigger.")]),
+    "9-place": ExtractionPrompts(name="Country", description="The country where the event or situation occurs. Options:", examples=[],
+                                 options=[ResultOption(name="1-czech-republic", description="The event or situation occurs in the Czech Republic."),
+                                          ResultOption(name="2-slovakia", description="The event or situation occurs in Slovakia."),
+                                          ResultOption(name="3-poland", description="The event or situation occurs in Poland."),
+                                          ResultOption(name="4-germany", description="The event or situation occurs in Germany."),
+                                          ResultOption(name="5-russia", description="The event or situation occurs in Russia."),
+                                          ResultOption(name="6-ukraine", description="The event or situation occurs in Ukraine."),
+                                          ResultOption(name="9-other", description="The event or situation occurs in other country."),
+                                          ResultOption(name="0-unknown", description="The event or situation occurs in unknown country.")]),
 }
