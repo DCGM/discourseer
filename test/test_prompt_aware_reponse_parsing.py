20a7ae97479e6a603ecebc6fb87a45520ea33408
vlachvojta <vojtik.vlach@seznam.cz>
2024-06-04 16:29:07 +0200

    Change question ids + answers to new format in tests.

--- test/test_prompt_aware_reponse_parsing.py
+++ test/test_prompt_aware_reponse_parsing.py
@@ -9,8 +9,8 @@ class ResponseParsing(unittest.TestCase):
 """
 responses seen so far:
  (what model responded) -> (what he meant, was one of the options)
- ukraine -> 6-ukraine
- 6 -> 6-ukraine
+ ukraine -> ukraine
+ 6 -> ukraine
  {'3': 1} -> 3    (range question, maybe not relevant)
  ... TODO add examples, see data/outputs
  ... TODO add option 'other' for unsuccessful paring??
