3c0645976a5fde9df48367d8153049a402d14571
vlachvojta <vojtik.vlach@seznam.cz>
2024-07-12 15:58:33 +0200

    Add experiment with details: IRR for each option.

---
+++ experiments/gaza_coding_FSS_details/run_experiment.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# get location of this script
+EXPERIMENT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+# if $DISCOURSEER variable is not set, default to current directory
+if [ -z $DISCOURSEER ]; then
+    DISCOURSEER="./"
+fi
+
+
+# TODO: First download csv files from Google Spreadsheet 
+# and save them in `ratings_GSpreadsheet_csv` folder
+
+# --------------- Converting ratings from Google Spreadsheet to CSV ratings  -----------
+RATINGS=$EXPERIMENT_DIR/ratings
+python $DISCOURSEER/GSpreadsheet_to_ratings.py \
+    $EXPERIMENT_DIR/ratings_GSpreadsheet_csv/ \
+    $RATINGS
+
+# --------------- Calculating Inter-rater reliability  -----------
+
+python $DISCOURSEER/calculate_IRR.py \
+    --ratings-dir $RATINGS \
+    --output-dir $EXPERIMENT_DIR/output_IRR/ \
+    --prompt-definitions $EXPERIMENT_DIR/prompt_definitions.json
+
