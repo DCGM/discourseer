c1edc07ba5c3253c2942561c20abedacf762375c
vlachvojta <vojtik.vlach@seznam.cz>
2025-03-04 10:07:00 +0100

    Add test format strings in unittest

--- test/test_experiment_setup.py
+++ test/test_experiment_setup.py
@@ -7,7 +7,7 @@ from discourseer.chat_client import Conversation
 from discourseer import utils
 
 
-class TestCodebooks(unittest.TestCase):
+class TestExperimentSetup(unittest.TestCase):
     discourseer_root = os.path.join(os.path.dirname(__file__), '..')
 
     def test_codebooks(self):
@@ -39,13 +39,16 @@ class TestCodebooks(unittest.TestCase):
         prompt_schemas = os.listdir(prompt_schema_dir)
         prompt_schemas = [p for p in prompt_schemas if p.endswith('.json')]
 
-        for prompt_schema in prompt_schemas:
+        reference_codebook_file = os.listdir(os.path.join(self.discourseer_root, 'codebooks'))
+        reference_codebook_file = [c for c in reference_codebook_file if c.endswith('.json') and 'gaza' in c][0]
+        reference_codebook = utils.load_codebook(os.path.join(self.discourseer_root, 'codebooks', reference_codebook_file))
+
+        for prompt_schema_file in prompt_schemas:
             try:
-                prompt_schema_path = os.path.join(prompt_schema_dir, prompt_schema)
+                prompt_schema_path = os.path.join(prompt_schema_dir, prompt_schema_file)
                 prompt_schema = Discourseer.load_prompt_schema_definition('', prompt_schema_path)
             except ValueError as e:
-                print(f'Failed to load {prompt_schema} with error: {e}')
-                raise e
+                raise ValueError(f'Failed to load {prompt_schema} with error: {e}')
 
             assert isinstance(prompt_schema, Conversation)
             self.assertTrue(hasattr(prompt_schema, 'messages'))
@@ -53,10 +56,16 @@ class TestCodebooks(unittest.TestCase):
 
             # test that prompt_schema has attributes of Conversation: model, max_tokens, temperature, top_p, response_format
             self.assertTrue(hasattr(prompt_schema, 'model'))
-            self.assertTrue(hasattr(prompt_schema, 'max_tokens'))
-            self.assertTrue(hasattr(prompt_schema, 'temperature'))
-            self.assertTrue(hasattr(prompt_schema, 'top_p'))
-            self.assertTrue(hasattr(prompt_schema, 'response_format'))
+            self.assertTrue(hasattr(prompt_schema, 'messages'))
+            self.assertTrue(len(prompt_schema.messages) > 0)
+
+            # test adding format strings to every message
+            for message in prompt_schema.messages:
+                try:
+                    message.content = message.content.format(**reference_codebook.get_format_strings(), text='test text')
+                except KeyError as e:
+                    raise KeyError(f'Format string {e} found in {prompt_schema_file} but is not defined in the codebook.')
+
 
 if __name__ == '__main__':
     unittest.main()
