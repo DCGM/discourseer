2555bae48e736322df5523190ada35217b9bb90b
Martin Kostelnik <martinkostelnik0@gmail.com>
2026-06-11 10:39:18 +0200

    Backoff on chunked exception

--- discourseer/chat_client.py
+++ discourseer/chat_client.py
@@ -137,7 +137,7 @@ class ChatClient:
             else:
                 return self.completions_with_backoff(**kwargs)
 
-    @backoff.on_exception(backoff.expo, (openai.RateLimitError, OpenRouterError, requests.exceptions.JSONDecodeError), max_time=300)
+    @backoff.on_exception(backoff.expo, (openai.RateLimitError, OpenRouterError, requests.exceptions.JSONDecodeError, requests.exceptions.ChunkedEncodingError), max_time=300)
     def completions_with_backoff(self, **kwargs):
         if self.openrouter:
             result = requests.post(
